967c4c1954417496550a4b2dff46ae36b13ee2af
Katie Williams <kawilliams8@gmail.com>
2025-08-04 10:40:01 -0600

    Mimic other repo, fix imports

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import './bootstrap.scss'
+
+import { createBootstrap } from 'bootstrap-vue-next'
+
+const app = createApp(App)
+app.use(createBootstrap())
+app.mount('#app')
